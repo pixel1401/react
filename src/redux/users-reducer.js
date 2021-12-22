90420d266209c7247be10bd24bca4eee8b115f05
Pixel <pixsel1401@gmail.com>
2021-12-22 20:53:04 +0500

    53 lesson , class Component

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -5,6 +5,17 @@ export const followAction = (value, id) => ({ type: FOLLOW, bool: value, id: id 
 export const setUsersAction = (usersArr) => ({ type: SET_USERS, usersArr });
 
 
+// props.setUsersAction(
+        //     [
+        //         { id: 1, name: "Erzhan", country: 'Kazakhstan', city: "Astana", quote: "It's my life", status: "Follow" },
+        //         { id: 2, name: "Chan-long", country: 'China', city: "Pekin", quote: "lorem lorem lore", status: "Unfollow" },
+        //         { id: 3, name: "Naruto", country: 'Japan', city: "Tokyo", quote: "It's my life aw dwad a", status: "Follow" },
+        //         { id: 4, name: "Oleg-Mongol ))", country: 'Russia', city: "Moscow", quote: "I am from Russia", status: "Follow" },
+        //         { id: 5, name: "Mike Jordan", country: 'USA', city: "Texas", quote: "I dont from Russia", status: "Follow" },
+        //         { id: 6, name: "Dina", country: 'Russia', city: "Moscow", quote: "I am from Russia", status: "UnFollow" },
+        //     ]
+        // )
+
 let userState = {
     usersPage: []
 }
